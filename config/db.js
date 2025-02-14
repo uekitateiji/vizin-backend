1a5d20760ba9f3ed351c85744d9c5f4763315f11
uekitateiji <uekita.teiji@gmail.com>
2025-02-14 20:31:15 -0300

    chore: adding rules for db

--- config/db.js
+++ config/db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false // Necessário para conexões seguras no Railway
+  }
 });
 
 module.exports = pool;
